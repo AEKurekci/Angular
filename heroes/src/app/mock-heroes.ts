7443fba26c73a0706890f3d9fe5ea2599f029309
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2020-05-15 14:39:26 +0300

    heroes initial

---
+++ heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: "Hz Muhammed"},
+    {id: 2, name: "Ömer"},
+    {id: 3, name: "Ebu Bekir"},
+    {id: 4, name: "Osman"},
+    {id: 5, name: "Ali"},
+    {id: 6, name: "Fatih Sultan"},
+    {id: 7, name: "Mustafa Kemal"},
+    {id: 8, name: "Mimar Sinan"},
+    {id: 9, name: "Mevlana"}
+];
