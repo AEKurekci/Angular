039d911096a15618d1bab99694f682e4260d7332
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2020-06-09 21:28:30 +0300

    posts

--- ng6/src/app/users/users.component.ts
+++ ng6/src/app/users/users.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'] 
 })
 export class UsersComponent implements OnInit {
 
